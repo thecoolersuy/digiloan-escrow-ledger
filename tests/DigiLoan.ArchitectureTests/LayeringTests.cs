63d9fb649868222447fd16326b5472a5ef8d167e
Suyal Sahukhal <suyalsahukhala787@gmail.com>
2026-09-15 21:29:34 +0545

    test: enforce architecture rules for layer boundaries

--- tests/DigiLoan.ArchitectureTests/LayeringTests.cs
+++ tests/DigiLoan.ArchitectureTests/LayeringTests.cs
@@ -13,7 +13,7 @@ public class LayeringTests
         .NotHaveDependencyOnAny("DigiLoan.Application", "DigiLoan.Infrastructure", "DigiLoan.Api")
         .GetResult();
 
-        Assert.True(result.IsSuccessful, $"Domain has forbidden dependencies on: {string.Join(",", result.FailingTypes)}");
+        Assert.True(result.IsSuccessful, $"Domain has forbidden dependencies on: {string.Join(",", result.FailingTypeNames ?? Enumerable.Empty<string>())}");
     }
 
     [Fact]
@@ -21,9 +21,46 @@ public class LayeringTests
     {
         var result = Types.InAssembly(typeof(Application.Services.CreditScoringService).Assembly)
           .Should()
-          .NotHaveDependencyOnAny("Domain.Infrastructure", "Domain.Api")
+          .NotHaveDependencyOnAny("DigiLoan.Infrastructure", "DigiLoan.Api")
           .GetResult();
 
-        Assert.True(result.IsSuccessful, $"Infrastructure has dependencies on {string.Join(",", result.FailingTypes)}");
+        Assert.True(result.IsSuccessful, $"Application has dependencies on {string.Join(",", result.FailingTypeNames ?? Enumerable.Empty<string>())}");
+    }
+
+    [Fact]
+    public void Infrastructure_ShouldNotDependOnApi()
+    {
+        var result = Types.InAssembly(typeof(Infrastructure.Persistence.AppDbContext).Assembly)
+         .Should()
+         .NotHaveDependencyOnAny("DigiLoan.Api")
+         .GetResult();
+
+        Assert.True(result.IsSuccessful, $"Infrastructure has dependencies on ${string.Join(",", result.FailingTypeNames ?? Enumerable.Empty<string>())}");
+    }
+
+    [Fact]
+    public void Repositories_ShouldOnlyLiveInInfrastructure()
+    {
+        var result = Types.InAssembly(typeof(Infrastructure.Persistence.AppDbContext).Assembly)
+            .That()
+            .HaveNameEndingWith("Repository")
+            .Should()
+            .ResideInNamespace("DigiLoan.Infrastructure.Repositories")
+            .GetResult();
+
+        Assert.True(result.IsSuccessful, $"Repository doesnt reside on Infrastructure. Violations in : {string.Join(",", result.FailingTypeNames ?? Enumerable.Empty<string>())}");
+    }
+
+    [Fact]
+    public void Controllers_ShouldOnlyResideInApi()
+    {
+        var result = Types.InAssembly(typeof(Api.Controllers.AuthController).Assembly)
+            .That()
+            .HaveNameEndingWith("Controller")
+            .Should()
+            .ResideInNamespace("DigiLoan.Api.Controllers")
+            .GetResult();
+
+        Assert.True(result.IsSuccessful, $"Controllers should only reside on Api. Violations in {string.Join(",", result.FailingTypeNames ?? Enumerable.Empty<string>())}");
     }
 }
